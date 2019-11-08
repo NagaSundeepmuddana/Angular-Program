17b66151f06f898d701e550ac8cc4fc1821974b4
Sundeep Muddana <SMuddana@railcarrx.com>
2019-11-08 22:03:58 +0530

    changes

--- firstapp/src/app/products/product.component.ts
+++ firstapp/src/app/products/product.component.ts
@@ -51,7 +51,51 @@ export class ProductComponent {
           },
           {
             _id: '5a05dacc734d1d68d42d31f3',
-            productId: 1,
+            productId: 3,
+            "productName": "Leaf test Rake",
+            "productCode": "GDN-0011",
+            "releaseDate": "March 1, 2016",
+            "description": "Leaf rake with 48-inch wooden handle.",
+            "price": 19.95,
+            "starRating": 3.5,
+            "imageUrl": "http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
+          },
+          {
+            "_id": "5a05daec734d1d68d42d32ca",
+            "productId": 4,
+            "productName": "Garden Cart 1",
+            "productCode": "GDN-0023",
+            "releaseDate": "March 9, 2016",
+            "description": "15 gallon capacity rolling garden cart",
+            "price": 32.99,
+            "starRating": 4.2,
+            "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
+          },
+          {
+            _id: '5a05dacc734d1d68d42d31f3',
+            productId: 5,
+            "productName": "Leaf Rake",
+            "productCode": "GDN-0011",
+            "releaseDate": "March 19, 2016",
+            "description": "Leaf rake with 48-inch wooden handle.",
+            "price": 19.95,
+            "starRating": 3.5,
+            "imageUrl": "http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
+          },
+          {
+            "_id": "5a05daec734d1d68d42d32ca",
+            "productId": 6,
+            "productName": "Garden Cart",
+            "productCode": "GDN-0023",
+            "releaseDate": "March 18, 2016",
+            "description": "15 gallon capacity rolling garden cart",
+            "price": 32.99,
+            "starRating": 4.2,
+            "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
+          },
+          {
+            _id: '5a05dacc734d1d68d42d31f3',
+            productId: 7,
             "productName": "Leaf Rake",
             "productCode": "GDN-0011",
             "releaseDate": "March 19, 2016",
